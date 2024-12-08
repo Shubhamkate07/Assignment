f545a7d688f71e3d01a7f667b258d2572448edeb
shubham kate <shubhamkate962@gmail.com>
2024-12-08 16:46:02 +0530

    changes

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -5,7 +5,7 @@ const Nav = () => {
   return (
     <nav className="nav-container">
       <div className="logo">
-        <a href="/">
+        <a href="/item">
           <img
             src="https://plus.unsplash.com/premium_photo-1677187301660-5e557d9c0724?q=80&w=1565&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             alt="img"
